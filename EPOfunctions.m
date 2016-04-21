61a0b5e21929bfe5c1fab8b93ed49282d525162e
Joran Out <joranlol@gmail.com>
2016-04-21 23:00:37 +0200

    status finished

--- EPOfunctions.m
+++ EPOfunctions.m
@@ -13,28 +13,40 @@ methods(Static)
             switch char(getdata(i))
                 case 'Beacon:'
                     audioBeacon = getdata(find(strcmp('Beacon:',getdata))+1);
+                    set(handles.audioBeacon,'String',audioBeacon);
                 case 'c:'
                     c = getdata(find(strcmp('c:',getdata))+1);
+                    set(handles.c ,'String',c);
                 case 'f_c:'
                     f_c = getdata(find(strcmp('f_c:',getdata))+1);
+                    set(handles.f_c ,'String',f_c);
                 case 'f_b:'
                     f_b = getdata(find(strcmp('f_b:',getdata))+1);
+                    set(handles.f_b ,'String',f_b);
                 case 'c_r:'
                     c_r = getdata(find(strcmp('c_r:',getdata))+1);
+                    set(handles.c_r ,'String',c_r);
                 case 'PWM:'
-                    PMW = getdata(find(strcmp('PWM:',getdata))+1); %todo
+                    PWM = getdata(find(strcmp('PWM:',getdata))+1); %todo
+                    set(handles.PWM ,'String',PWM);
                 case 'Dir.'
                     Dir = getdata(find(strcmp('Dir.',getdata))+1);
+                    set(handles.Dir ,'String',Dir);
                 case 'Mot.'
                     Mot = getdata(find(strcmp('Mot.',getdata))+1);
+                    set(handles.Mot ,'String',Mot);
                 case 'Sensors:'
                     Sensors = getdata(find(strcmp('Sensors:',getdata))+1); %todo
+                    set(handles.Sensors ,'String',Sensors);
                 case 'L'
                     distL = getdata(find(strcmp('L',getdata))+1);
+                    set(handles.distL ,'String',distL);
                 case 'R'
                     distR = getdata(find(strcmp('R',getdata))+1);
+                    set(handles.distR ,'String',distR);
                 case 'V_batt'
                     vBatt = getdata(find(strcmp('V_batt',getdata))+1);
+                    set(handles.V_batt ,'String',vBatt);
                 otherwise
             end
             i=i+1;
