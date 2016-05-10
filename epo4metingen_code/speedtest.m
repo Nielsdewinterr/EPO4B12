78f013137da01017682de8c4a6b2581cc5cb472c
Nielsdewinterr <niels@dewinterr.nl>
2016-05-10 09:50:24 +0200

    codes voor verslag

--- epo4metingen_code/speedtest.m
+++ epo4metingen_code/speedtest.m
@@ -1,15 +1,9 @@
-status = EPOCommunications('transmit','M157')
+
 status = EPOCommunications('transmit','D150')
 while(1)
-  
-    status = EPOCommunications('transmit','Sd');
-    dl=status(4:6);
-    x=str2num(dl);
-    if(x <150)
-       status = EPOCommunications('transmit','M135')
-       pause(0.3);
-       status = EPOCommunications('transmit','M150')
-       break
-   
-    end
+status = EPOCommunications('transmit','M160')
+wait(3)
+status = EPOCommunications('transmit','m140') 
+wait(3)
 end
+
