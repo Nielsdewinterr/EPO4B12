e10ceb4342ab1be7f63b66f6b753fd2df871a6a4
KKlein96 <keith-klein@live.com>
2016-05-20 15:43:45 +0200

    mainfile data extract Tdoa en lineare algebra

---
+++ Mainfile.m
@@ -0,0 +1,16 @@
+[ref1]=extract_data(RXXr,1);
+ref1 = ref1(200:3500);
+
+%   for i = 6:11
+i=7;
+[a,b,c,d,e]=extract_data(RXXr,i);
+[r12,r13,r14,r15,r23,r24,r25,r34,r35,r45]=TDOA(a,b,c,d,e,ref1);
+[x,y] = LIN (r12,r13,r14,r23,r24,r34);
+plot(y,x,'x');
+a=num2str(i);
+text(y,x,a)
+hold on;
+% xlim([0,5]);
+% ylim([0,5]);
+%   end
+
