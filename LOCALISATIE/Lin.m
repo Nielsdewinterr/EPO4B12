baf67c8d3d87d395436a854df96cf15cceae3b0d
Nielsdewinterr <Niels@wlan-145-94-181-77.wlan.tudelft.nl>
2016-05-24 09:49:28 +0200

    final localisatie

--- LOCALISATIE/Lin.m
+++ LOCALISATIE/Lin.m
@@ -1,10 +1,10 @@
- function [x,y] = Lin (r12,r13,r14,r23,r24,r34)
+function [x,y] = LIN (r12,r13,r14,r23,r24,r34)
 
-
-x1 = [0;5];
-x2 = [5;5];
-x3 = [5;0];
-x4 = [0;0];
+x1 = [0;4.14];
+x2 = [0;0];
+x3 = [4.53;0];
+x4 = [4.53;4.14];
+% x5 = [2.27;0];
 
 b=[(r12)^2-norm(x1)^2+norm(x2)^2;
     (r13)^2-norm(x1)^2+norm(x3)^2;
@@ -19,6 +19,5 @@ A=[(2*transpose(x2-x1)) (-2*r12) 0 0;
     2*transpose(x4-x2) 0 0 -2*r24;
     2*transpose(x4-x3) 0 0 -2*r34;];
 Y=pinv(A)*b
-x=Y(2)
-y=Y(1)
-% diff=sqrt((x-auto(1))^2+(y-auto(2))^2)
+x=Y(2);
+y=Y(1);
