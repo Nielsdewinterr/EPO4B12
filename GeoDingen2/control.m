96d24244975b4f01c53f603d571e20de111c72b9
Joran Out <joranlol@gmail.com>
2016-06-02 11:25:48 +0200

    working back-off protocol and 180+turns

--- GeoDingen2/control.m
+++ GeoDingen2/control.m
@@ -1,10 +1,12 @@
-function [turntime,orientation,lr,straighttime]=control(x,y,rot,xdest,ydest)
+function [turntime,orientation,lr,straighttime,OoF]=control(x,y,rot,xdest,ydest,OoFc)
 %this function assembles the car coordinates (x,y), the destination
 %coordinates(xdest,ydest) and the rotation of the car(rot - in degrees).
 %Its output is either 0, which means the car must sally straight on, or a
 %vector containing the time the car must turn (turntime), the orientation
 %the car has after the turn (orientation), whether the car must turn left
-%or right(lr) and how long the car should travel straight on.
+%or right(lr) and how long the car should travel straight on. Furthermore,
+%it calculates if the path is out of field and calculates how close he is
+%(OoFd)
 %The variables used frequently inside this function are:
 
 %R: radius circle
@@ -13,8 +15,11 @@ function [turntime,orientation,lr,straighttime]=control(x,y,rot,xdest,ydest)
 %D: destination point
 %T: Target point on circle
 tic;
+fieldx=4.53;
+fieldy=4.14;
 speedcirkel=0.74;%origional:0.52
 speedrecht=0.79;%origional:1.19
+speedback=0.79;
 R=0.9;%origional:0.925
 D=[xdest,ydest];
 C=[x,y];
@@ -36,10 +41,11 @@ end
 % place
 % D(2)>C(2)
 % sind(rot)<=0
-if (place == 0 || isnan(place)) && (xor(D(2)>C(2),sind(rot)<=0))          %straight ahead
+if (place == 0 || isnan(place)) && (xor(D(2)>C(2),sind(rot)<0))          %straight ahead
     lr = 0;
     orientation=rot;
     turntime=0;
+    OoF=0;
     straighttime = abs((sqrt((C(1)-D(1))^2+(C(2)-D(2))^2))/speedrecht);
 
     %plot all
@@ -48,20 +54,21 @@ if (place == 0 || isnan(place)) && (xor(D(2)>C(2),sind(rot)<=0))          %strai
     hold on;
     scatter(D(1),D(2))
     text(D(1)+0.15,D(2),'Destination');
-    xlim([-1 5]);
-    ylim([-1 5]);
+    xlim([0 fieldx]);
+    ylim([0 fieldy]);
 else
     if place > 0        %turn right
         lr=-1;
         Mx=x+R*cosd(90+rot);
         My=y+R*sind(90+rot);
-        M=[Mx,My]
+        M=[Mx,My];
     else                %turn left
         lr=1;
         Mx=x-R*cosd(90+rot);
         My=y-R*sind(90+rot);
-        M=[Mx,My]
-    end
+        M=[Mx,My];
+    end    
+    
     %calculate length Dest-Target
     lengthDM= (sqrt((M(1)-D(1))^2+(M(2)-D(2))^2));
     %calculate rotation D
@@ -73,83 +80,164 @@ else
     straighttime=distDT/speedrecht;
     %calculate DX
     angleMD=atan2d((D(1)-M(1)),(D(2)-M(2)));
-   
-
-    %Now we nearly have the points on the circle, but instead of two points we need to
-    %choose the one closest to the front of the car. A bonus development may be riding
-    %backward if the second value is close behind the car.
-
-    %To calculate the angles on the circle we use the dot-product rules
-    VectorMC=[(M(1)-C(1)),(M(2)-C(2))];
-    LengthMC=norm(VectorMC,2);
-    if lr==1
-        DX2=[-distDT*sind(rotMDX+angleMD),-distDT*cosd(rotMDX+angleMD)];
-        T2 = D + DX2
-        VectorMT2=[(M(1)-T2(1)),(M(2)-T2(2))];
-        LengthMT2=norm(VectorMT2,2);
-        if place == 0 || isnan(place) %handle backwards driving
-            AngleCT2 = real(acosd(dot(VectorMC,VectorMT2)/(LengthMC*LengthMT2)))
-        else
-            AngleCT2 = -real(acosd(dot(VectorMC,VectorMT2)/(LengthMC*LengthMT2)))
-        end
-        turntime = abs((2*pi*AngleCT2/360)*R/speedcirkel);
-        orientation = rot+AngleCT2;
-    else
-        DX1=[-distDT*sind(-rotMDX+angleMD),-distDT*cosd(-rotMDX+angleMD)];
-        T1 = D + DX1
-        VectorMT1=[(M(1)-T1(1)),(M(2)-T1(2))];
-        LengthMT1=norm(VectorMT1,2);
-        if place == 0 || isnan(place) %handle backwards driving
-            AngleCT1 = real(-acosd(dot(VectorMC,VectorMT1)/(LengthMC*LengthMT1)))
-        else
-            AngleCT1 = real(acosd(dot(VectorMC,VectorMT1)/(LengthMC*LengthMT1)))
-        end
-        turntime = abs((2*pi*AngleCT1/360)*R/speedcirkel);
-        orientation = rot+AngleCT1;
-    end
-
-    toc;
+    %detect going out of field
     
-    %plot circle
-    ang=0:0.01:2*pi; 
-    xc=R*cos(ang);
-    yc=R*sin(ang);
-    plot(M(1)+xc,M(2)+yc,'--');
-    hold on;
-    %plot current heading
-    timea=-1:1:5;
-    ya=(tand(rot))*(timea-C(1))+C(2);
-    plot(timea,ya,':');
-    hold on;
-    %plot all
-    scatter(C(1),C(2))
-    text(C(1)+0.15,C(2),'Car');
-    hold on;
-    if lr==1
-        scatter(T2(1),T2(2))
-        text(T2(1)+0.15,T2(2),'Target2');
+    back=[0 0 0 0];
+    if (M(1)<(0-R*cos(rot))||M(1)>(fieldx-R)||M(2)<(0+R)||M(2)>(fieldy-R))
+        if M(1)<(0-R*cos(rot))
+            %circle crosses field line on the left
+            back(1)=-(R-M(1))/cos(rot);
+        end
+        if M(1)>(fieldx-R)
+            %circle crosses field line on the right
+            back(2)=(R-(fieldx-M(1)))/cos(rot);
+        end
+        if M(2)<(0+R)
+            %circle crosses bottom field line
+            back(3)=(R-M(2))/sin(rot);
+        end
+        if M(2)>(fieldy-R)
+            %circle crosses top field line
+            back(4)=(R-(fieldy-M(2)))/sin(rot);
+        end
+        turntime=0;
+        orientation=rot;
+        lr=0;
+        OoF=1;
+        straighttime=(max(back)+0.1)/speedback;
+        
+        %plot all
+        scatter(C(1),C(2))
+        text(C(1)+0.15,C(2),'Car');
         hold on;
-        %plot next heading
-        timen=-1:1:5;
-        yn=(tand(orientation))*(timen-T2(1))+T2(2);
-        plot(timen,yn,':');
-        hold on;
+        scatter(D(1),D(2))
+        text(D(1)+0.15,D(2),'Destination');
+        xlim([0 fieldx]);
+        ylim([0 fieldy]);
     else
-        scatter(T1(1),T1(2))
-        text(T1(1)+0.15,T1(2),'Target1');
-        hold on;
-        %plot next heading
-        timen=-1:1:5;
-        yn=(tand(orientation))*(timen-T1(1))+T1(2);
-        plot(timen,yn,':');
-        hold on;
-    end
-    scatter(M(1),M(2))
-    text(M(1)+0.15,M(2),'Circle');
-    hold on;
-    scatter(D(1),D(2))
-    text(D(1)+0.15,D(2),'Destination');
-    xlim([-1 5]);
-    ylim([-1 5]);
+        OoF=0;
+        %Now we nearly have the points on the circle, but instead of two points we need to
+        %choose the one closest to the front of the car. A bonus development may be riding
+        %backward if the second value is close behind the car.
+        
+       
+        %To calculate the angles on the circle we use the dot-product rules
+        VectorMC=[(M(1)-C(1)),(M(2)-C(2))];
+        LengthMC=norm(VectorMC,2);
+        if lr==1 %turn right
+            DX2=[-distDT*sind(rotMDX+angleMD),-distDT*cosd(rotMDX+angleMD)];
+            T2 = D + DX2;
+            VectorMT2=[(M(1)-T2(1)),(M(2)-T2(2))];
+            LengthMT2=norm(VectorMT2,2);
+            %detect 180+ turn
+            if sind(rot)==0 
+                place = M(1)-T2(1)
+            else
+                if (sind(rot)<0) 
+                    place = round((((T2(2)-(2*M(2)-C(2)))/tand(rot+90))+((2*M(1)-C(1))-T2(1))),1)
+                    3
 
+                    timea=-1:1:5;
+                    ya=(tand(rot+90))*(timea-(2*M(1)-C(1)))+(2*M(2)-C(2));
+                    plot(timea,ya,':');
+                    hold on;
+                else
+                    place = round(-((T2(2)-(2*M(2)-C(2)))/tand(rot+90))+((2*M(1)-C(1))-T2(1)),1)
+                    4
+                    timea=-1:1:5;
+                    ya=(tand(rot+90))*(timea-(2*M(1)-C(1)))+(2*M(2)-C(2));
+                    plot(timea,ya,':');
+                    hold on;
+                end
+            end
+            if  place > 0 %handle 180+ degree turning
+                AngleCT2 = -real(acosd(dot(VectorMC,VectorMT2)/(LengthMC*LengthMT2)))
+                5
+            else
+                AngleCT2 = real(acosd(dot(VectorMC,VectorMT2)/(LengthMC*LengthMT2)))
+                6
+            end
+            turntime = (2*pi*AngleCT2/360)*R/speedcirkel;
+            orientation = rot+AngleCT2;
+        else %turn left
+            DX1=[-distDT*sind(-rotMDX+angleMD),-distDT*cosd(-rotMDX+angleMD)];
+            T1 = D + DX1;
+            VectorMT1=[(M(1)-T1(1)),(M(2)-T1(2))];
+            LengthMT1=norm(VectorMT1,2);
+            %detect 180+ turn
+            if sind(rot)==0 
+                place = M(1)-T1(1)
+            else
+                if (sind(rot)<0) 
+                    place = round((((T1(2)-(2*M(2)-C(2)))/tand(rot+90))+((2*M(1)-C(1))-T1(1))),1)
+                    3
+
+                    timea=-1:1:5;
+                    ya=(tand(rot+90))*(timea-(2*M(1)-C(1)))+(2*M(2)-C(2));
+                    plot(timea,ya,':');
+                    hold on;
+                else
+                    place = round(-((T1(2)-(2*M(2)-C(2)))/tand(rot+90))+((2*M(1)-C(1))-T1(1)),1)
+                    4
+                    timea=-1:1:5;
+                    ya=(tand(rot+90))*(timea-(2*M(1)-C(1)))+(2*M(2)-C(2));
+                    plot(timea,ya,':');
+                    hold on;
+                end
+            end
+            if xor(place > 0, sin(rot)<0)%handle 180+ degree turning
+                1
+                AngleCT1 = -real(-acosd(dot(VectorMC,VectorMT1)/(LengthMC*LengthMT1)))
+            else
+                2
+                AngleCT1 = real(-acosd(dot(VectorMC,VectorMT1)/(LengthMC*LengthMT1)))
+            end
+            turntime = (2*pi*AngleCT1/360)*R/speedcirkel;
+            orientation = rot+AngleCT1;
+        end
+
+        toc;
+
+        %plot circle
+        ang=0:0.01:2*pi; 
+        xc=R*cos(ang);
+        yc=R*sin(ang);
+        plot(M(1)+xc,M(2)+yc,'--');
+        hold on;
+        %plot current heading
+        timea=-1:1:5;
+        ya=(tand(rot))*(timea-C(1))+C(2);
+        plot(timea,ya,':');
+        hold on;
+        %plot all
+        scatter(C(1),C(2))
+        text(C(1)+0.15,C(2),'Car');
+        hold on;
+        if lr==1
+            scatter(T2(1),T2(2))
+            text(T2(1)+0.15,T2(2),'Target2');
+            hold on;
+            %plot next heading
+            timen=-1:1:5;
+            yn=(tand(orientation))*(timen-T2(1))+T2(2);
+            plot(timen,yn,':');
+            hold on;
+        else
+            scatter(T1(1),T1(2))
+            text(T1(1)+0.15,T1(2),'Target1');
+            hold on;
+            %plot next heading
+            timen=-1:1:5;
+            yn=(tand(orientation))*(timen-T1(1))+T1(2);
+            plot(timen,yn,':');
+            hold on;
+        end
+        scatter(M(1),M(2))
+        text(M(1)+0.15,M(2),'Circle');
+        hold on;
+        scatter(D(1),D(2))
+        text(D(1)+0.15,D(2),'Destination');
+        xlim([0 fieldx]);
+        ylim([0 fieldy]);
+    end
 end
--- GeoDingen2/testControlDrive.m
+++ GeoDingen2/testControlDrive.m
@@ -2,9 +2,20 @@ function [x,y,orientation] = testControlDrive(x,y,rot,xdest,ydest)
 
 speedcirkel=0.74;%origional:0.52
 speedrecht=0.79;%origional:1.19
+speedback=0.79;
 R=0.9;%origional:0.925
 
-[turntime,orientation,lr,straighttime] = control(x,y,rot,xdest,ydest)
+[turntime,orientation,lr,straighttime,OoF] = control(x,y,rot,xdest,ydest,0)
+
+if OoF==1
+    %         EPOCommunications('transmit', 'D100');
+    %         EPOCommunications('transmit', 'M157');
+    pause(straighttime)
+    x = x - straighttime*speedback*cosd(orientation);
+    y = y - straighttime*speedback*sind(orientation);
+    input('continue?')
+    [turntime,orientation,lr,straighttime,OoF] = control(x,y,rot,xdest,ydest,1)
+end
 
 if rem(straighttime,1)>0.3
     turns = fix(straighttime/1);
@@ -17,42 +28,43 @@ for a = 1:1:turns
     if lr == -1 %turn left
 %         EPOCommunications('transmit', 'D100');
 %         EPOCommunications('transmit', 'M157');
-        pause(turntime)
-        x = x + R*cosd(rot+90)+R*cosd(rot-90+((turntime*speedcirkel*360)/(2*pi)))
-        y = y + R*sind(rot+90)+R*sind(rot-90+((turntime*speedcirkel*360)/(2*pi)))
+        pause(abs(turntime))
+        x = x + R*cosd(rot+90)+R*cosd(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
+        y = y + R*sind(rot+90)+R*sind(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
     elseif lr == 1 %turn right
 %         EPOCommunications('transmit', 'D200');
 %         EPOCommunications('transmit', 'M157');  
-        pause(turntime)
-        x = x + R*cosd(rot-90)+R*cosd(rot+90-((turntime*speedcirkel*360)/(2*pi*R)))
-        y = y + R*sind(rot-90)+R*sind(rot+90-((turntime*speedcirkel*360)/(2*pi*R)))
+        pause(abs(turntime))
+        x = x + R*cosd(rot-90)+R*cosd(rot+90+((turntime*speedcirkel*360)/(2*pi*R)));
+        y = y + R*sind(rot-90)+R*sind(rot+90+((turntime*speedcirkel*360)/(2*pi*R)));
+        1
     else %no turn
     end   
 %     EPOCommunications('transmit', 'D150');
 %     EPOCommunications('transmit', 'M157');
     pause(1)
     %drive forward
-    x = x + speedrecht*cosd(orientation);
-    y = y + speedrecht*sind(orientation);
+    x = x + speedrecht*cosd(orientation)
+    y = y + speedrecht*sind(orientation)
 %     EPOCommunications('transmit', 'M150'); 
     %check again
     %here comes the TDOA TEST
     input('continue?')
-    [turntime,orientation,lr,straighttime] = control(x,y,orientation,xdest,ydest)
+    [turntime,orientation,lr,straighttime,OoF] = control(x,y,orientation,xdest,ydest,0)
 end
 %turn and drive
 if lr == -1 %turn left
 %     EPOCommunications('transmit', 'D100');
 %     EPOCommunications('transmit', 'M157');
-    pause(turntime)
-    x = x + R*cosd(rot+90)+R*cosd(rot-90+((turntime*speedcirkel*360)/(2*pi)));
-    y = y + R*sind(rot+90)+R*sind(rot-90+((turntime*speedcirkel*360)/(2*pi)));
+    pause(abs(turntime))
+    x = x + R*cosd(rot+90)+R*cosd(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
+    y = y + R*sind(rot+90)+R*sind(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
 elseif lr == 1 %turn right
 %     EPOCommunications('transmit', 'D200');
 %     EPOCommunications('transmit', 'M157');  
-    pause(turntime)
-    x = x + R*cosd(rot-90)+R*cosd(rot+90-((turntime*speedcirkel*360)/(2*pi)));
-    y = y + R*sind(rot-90)+R*sind(rot+90-((turntime*speedcirkel*360)/(2*pi)));
+    pause(abs(turntime))
+    x = x + R*cosd(rot-90)+R*cosd(rot+90-((turntime*speedcirkel*360)/(2*pi*R)));
+    y = y + R*sind(rot-90)+R*sind(rot+90-((turntime*speedcirkel*360)/(2*pi*R)));
 else %no turn
 end   
 % EPOCommunications('transmit', 'D150');
