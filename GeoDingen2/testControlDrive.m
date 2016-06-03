4fd7c88bbc1315cccb15a776c9ab614daac415aa
Joran Out <joranlol@gmail.com>
2016-06-04 01:05:56 +0200

    bijna helemaal werkend

--- GeoDingen2/testControlDrive.m
+++ GeoDingen2/testControlDrive.m
@@ -28,42 +28,43 @@ for a = 1:1:turns
     if lr == -1 %turn left
 %         EPOCommunications('transmit', 'D100');
 %         EPOCommunications('transmit', 'M157');
-        pause(abs(turntime))
+        pause(turntime)
         x = x + R*cosd(rot+90)+R*cosd(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
         y = y + R*sind(rot+90)+R*sind(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
     elseif lr == 1 %turn right
 %         EPOCommunications('transmit', 'D200');
 %         EPOCommunications('transmit', 'M157');  
-        pause(abs(turntime))
-        x = x + R*cosd(rot-90)+R*cosd(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
-        y = y + R*sind(rot-90)+R*sind(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
+        pause(turntime)
+        x = x + R*cosd(rot-90) + R*cosd(rot+90-((turntime*speedcirkel*360)/(2*pi*R)));
+        y = y + R*sind(rot-90) + R*sind(rot+90-((turntime*speedcirkel*360)/(2*pi*R)));
     else %no turn
     end   
 %     EPOCommunications('transmit', 'D150');
 %     EPOCommunications('transmit', 'M157');
     pause(1)
     %drive forward
-    x = x + speedrecht*cosd(orientation)
-    y = y + speedrecht*sind(orientation)
+    x = x + speedrecht*cosd(orientation);
+    y = y + speedrecht*sind(orientation);
 %     EPOCommunications('transmit', 'M150'); 
     %check again
     %here comes the TDOA TEST
     input('continue?')
-    [turntime,orientation,lr,straighttime,OoF] = control(x,y,orientation,xdest,ydest,0)
+    rot=orientation;
+    [turntime,orientation,lr,straighttime,OoF] = control(x,y,rot,xdest,ydest,0)
 end
 %turn and drive
-if lr == -1 %turn left
-%     EPOCommunications('transmit', 'D100');
-%     EPOCommunications('transmit', 'M157');
-    pause(abs(turntime))
+ if lr == -1 %turn left
+%         EPOCommunications('transmit', 'D100');
+%         EPOCommunications('transmit', 'M157');
+    pause(turntime)
     x = x + R*cosd(rot+90)+R*cosd(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
     y = y + R*sind(rot+90)+R*sind(rot-90+((turntime*speedcirkel*360)/(2*pi*R)));
 elseif lr == 1 %turn right
-%     EPOCommunications('transmit', 'D200');
-%     EPOCommunications('transmit', 'M157');  
-    pause(abs(turntime))
-    x = x + R*cosd(rot-90)+R*cosd(rot+90-((turntime*speedcirkel*360)/(2*pi*R)));
-    y = y + R*sind(rot-90)+R*sind(rot+90-((turntime*speedcirkel*360)/(2*pi*R)));
+%         EPOCommunications('transmit', 'D200');
+%         EPOCommunications('transmit', 'M157');  
+    pause(turntime)
+    x = x + R*cosd(rot-90)+R*cosd(rot-90-((turntime*speedcirkel*360)/(2*pi*R)));
+    y = y + R*sind(rot-90)+R*sind(rot-90-((turntime*speedcirkel*360)/(2*pi*R)));
 else %no turn
 end   
 % EPOCommunications('transmit', 'D150');
