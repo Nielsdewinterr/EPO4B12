ae80860488bb84a81543607eaff2e52f52e7f4b2
tomaattje <h.kruijsse@yahoo.com>
2016-06-09 17:10:06 +0200

    9-6

---
+++ testen9-6eind/audio_recieve.m
@@ -0,0 +1,9 @@
+function inputbuffer = audio_recieve
+    channels = 5;
+    samplerate = 48000 ; %Sample rate default = 41100 
+    nsamples = 48000;    %the number of samples to record from each channel
+    deviceid = 0;        %device ID default = 0
+
+    inputbuffer = pa_wavrecord(1,5,nsamples, [samplerate], [deviceid], ['asio']);
+    
+end
---
+++ testen9-6eind/timedistance.m
@@ -0,0 +1,41 @@
+function dapprox = timedistance(V)
+
+%%metingen
+tmeting16=[0,0.5,1,1.5,2,2.5,3,5];
+dmeting16=[0,0.15,.3,.59,.90,1.24,1.63,5.35];
+
+tmeting179=[0,.5,1,1.5,2,2.5,3,5];
+dmeting179=[0,.3,.52,.91,1.39,2.03,2.66,5.35];
+
+tmeting189=[0,.5,1,1.5,2,2.5,3,5];
+dmeting189=[0,.35,.55,1.01,1.69,2.5,3.4,5.35];
+
+% plot(t,v)
+% hold on
+% plot(t,vt)
+for i=1:8;
+    D=interp1([16,17.9,18.9],[dmeting16(i),dmeting179(i),dmeting189(i)],[16:0.05:19],'spline');
+%     plot([16:0.05:19],D)
+%     hold on;
+    dapprox(i)=D(round((V-16)*20));
+end
+
+% % figure(2)
+% % hold on
+% % % plot(t,x)
+% % % hold on
+% % % plot(t,xt)
+% % % hold on
+% % scatter(tmeting16,dmeting16,'g')
+% % hold on
+% % scatter(tmeting179,dmeting179,'g')
+% % hold on
+% % scatter(tmeting189,dmeting189,'g')
+% % hold on
+% % plot([0:0.01:6],interp1(tmeting16,dmeting16,[0:0.01:6],'spline'),'g')
+% % hold on
+% % plot([0:0.01:6],interp1(tmeting179,dmeting179,[0:0.01:6],'spline'),'g')
+% % hold on
+% % plot([0:0.01:6],interp1(tmeting189,dmeting189,[0:0.01:6],'spline'),'g')
+% % hold on
+% % plot([0:0.01:6],interp1(tmeting189,dapprox,[0:0.01:6],'spline'),'red')
