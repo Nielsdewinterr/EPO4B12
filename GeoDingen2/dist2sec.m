33dca6c027a013be8b6a006e616de164fcaf250c
Joran Out <joranlol@gmail.com>
2016-06-09 11:30:47 +0200

    update metingen

--- GeoDingen2/dist2sec.m
+++ GeoDingen2/dist2sec.m
@@ -1,31 +1,12 @@
-function [ straighttime, turntime ] = dist2sec(straight,turn,OoF)
+function [ straighttime, turntime ] = dist2sec(straight,turn,OoF,dapprox)
 %astraight=0.88
 %bstraight=0.51
 vttheo=0.5;%origional:0.52
 vtheo=1.03;%origional:1.19
 
-t=[0:0.002:10];
 
-%straight constants
-a=1.03;
-b=0.91;
-c=log(a)/-b;
-% a=1.38;
-% b=0.82;
-% c=log(a)/-b;
-
-%turn constants
-at=0.5;
-bt=0.91;
-ct=log(at)/-b;
-
-%straight speed/distance
-v=a-exp(-b*(t+c));
-x=(a*t+1/b*exp(-b*(t+c)))-exp(-b*c)/b;
-
-%turn speed/distance
-vt=at-exp(-bt*(t+ct));
-xt=(at*t+1/bt*exp(-bt*(t+ct)))-exp(-bt*ct)/bt;
+x = interp1([0,0.5,1,1.5,2,2.5,3],dapprox,[0:0.01:3],'spline');
+singledistance = x(150);
 
 %% calculate time
 %straight
@@ -33,18 +14,18 @@ if OoF==1
     straighttmp=0;
     times=0;
 else
-    times=fix(straight/0.7);
+    times=fix(straight/singledistance);
     straighttmp = times*1.5;
 end
-tmp = abs(x-(straight-times*0.7));
+tmp = abs(x-(straight-times*singledistance));
 [~, straighttime]=min(tmp);
 straighttime=straighttime/500;
 straighttime=straighttime+straighttmp;
 
-%turn
-tmp = abs(xt-turn);
-[~, turntime]=min(tmp);
-turntime=turntime/500;
+% %turn
+% tmp = abs(xt-turn);
+% [~, turntime]=min(tmp);
+% turntime=turntime/500;
 
 %% plot
 % plot(t,v)
