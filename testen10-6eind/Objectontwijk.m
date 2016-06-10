a337bb5e749bbf9761cf26ba208cd8a62bed7403
Joran Out <joranlol@gmail.com>
2016-06-10 15:51:04 +0200

    test vrijdag

--- testen10-6eind/Objectontwijk.m
+++ testen10-6eind/Objectontwijk.m
@@ -1,13 +1,14 @@
-function [x,y] = Objectontwijk(x,y,rot)
-fieldx = 5;
-fieldy = 5;
+function [x,y,rot] = Objectontwijk(x,y,rot)
+fieldx = 5.50;
+fieldy = 5.70;
 vardist = 2;%afstand van muur bewaken
 distanceobject = 1;%afstand object
-if(x  > (fieldx - vardist) || x < vardist || y > (fieldy - vardist) || y < vardist) %sta ik  binnen een meter van de muur
-    %zo ja, dan moet ik gewoon door
-else
+% if(x  > (fieldx - vardist) || x < vardist || y > (fieldy - vardist) || y < vardist) %sta ik  binnen een meter van de muur
+%     %zo ja, dan moet ik gewoon door
+% else
     [distL,distR]=EPOfunctions.status();
-    [distL,distR]=[distL/100,distR/100]
+    distL=distL/100;
+    distR=distR/100;
     if((distL < distanceobject) && (distR < distanceobject))
         %object voor je
         Linksaf();
@@ -18,8 +19,10 @@ else
         punt2obj = obj + [0.2*cosd(rot-90),0.2*cosd(rot-90)]
         plot([punt1obj,punt2obj])
         %auto
-%         x = x + afstand*cos(rot+)
-%         rot = rot+45;
+        x = x + 1.20*cosd(rot+56);
+        y = y + 1.20*sind(rot+56);
+        rot = rot+10;
+
         
     elseif(distL < distanceobject )
         Rechtsaf(); 
@@ -30,6 +33,10 @@ else
         punt2obj = obj + [0.2*cosd(rot-90),0.2*cosd(rot-90)]
         plot([punt1obj,punt2obj])
         %links gezien? draai rechts
+        %auto
+        x = x + 1.20*cosd(rot-56);
+        y = y + 1.20*sind(rot-56);
+        rot = rot-10;
     elseif(distR < distanceobject)
         Linksaf();
         xobj = x + distR*cosd(rot)+0.3*cosd(rot-90);
@@ -39,10 +46,13 @@ else
         punt2obj = obj + [0.2*cosd(rot-90),0.2*cosd(rot-90)]
         plot([punt1obj,punt2obj])
         % object rechtsgezien, draai links
-        
+        %auto
+        x = x + 1.20*cosd(rot+56);
+        y = y + 1.20*sind(rot+56);
+        rot = rot+10;
     end
 end
-end
+% end
 
 function Linksaf()
 EPOCommunications('transmit','D200');
@@ -50,19 +60,20 @@ EPOCommunications('transmit','M157');
 pause(1.2)
 EPOCommunications('transmit','M158');
 EPOCommunications('transmit','D150');
-pause(1.2)
+pause(.6)
+EPOCommunications('transmit','M143');
+pause(.2)
 EPOCommunications('transmit','M150');
-EPOCommunications('transmit','D150');
 end
 
 function Rechtsaf()
 EPOCommunications('transmit','D100');
 EPOCommunications('transmit','M157');
 pause(1.2)
-EPOCommunications('transmit','M150');
-EPOCommunications('transmit','D150');
-pause(1.2)
 EPOCommunications('transmit','M158');
 EPOCommunications('transmit','D150');
+pause(.6)
+EPOCommunications('transmit','M143');
+pause(.2)
+EPOCommunications('transmit','M150');
 end
-
---
+++ testen10-6eind/testControlDrive.m
@@ -0,0 +1,192 @@
+function [x,y,orientation] = testControlDrive(x,y,rot,xdest,ydest,ref,curve,challenge)
+clf;
+speedcirkel=0.74;%origional:0.52
+speedrecht=0.79;%origional:1.19
+timetheorecht=0.7/speedrecht;
+speedback=0.79;
+R=0.85;%origional:0.925
+
+[turntime,turntimetheo,orientation,lr,straighttime,straighttimetheo,OoF] = control(x,y,rot,xdest,ydest,curve);
+    [x,y]=tdoatest(x,y,ref);
+    if (checkdistance(x,y,xdest,ydest)==1)
+       return; 
+    end
+    if (challenge==3)
+        [x,y,rot]=Objectontwijk(x,y,rot);
+    end
+
+if OoF==1
+            EPOCommunications('transmit', 'D150');
+            EPOCommunications('transmit', 'M143');
+    pause(straighttime)
+    x = x - straighttimetheo*speedback*cosd(orientation);
+    y = y - straighttimetheo*speedback*sind(orientation);
+            EPOCommunications('transmit', 'M158');
+            pause(0.3)
+            EPOCommunications('transmit', 'M150');
+    [x,y]=tdoatest(x,y,ref)
+        if (checkdistance(x,y,xdest,ydest)==1)
+           return; 
+        end
+    if (challenge==3)
+        [x,y,rot]=Objectontwijk(x,y,rot);
+    end
+    [turntime,turntimetheo,orientation,lr,straighttime,straighttimetheo,OoF] = control(x,y,rot,xdest,ydest,curve);
+end
+
+if rem(straighttime,1.5)>0.3
+    turns = fix(straighttime/1.5);
+else
+    turns = (fix(straighttime/1.5))-1.5;
+end
+
+    for a = 1:1:turns
+        %turn and drive
+        if lr == -1 %turn left
+            EPOCommunications('transmit', 'D200');
+            EPOCommunications('transmit', 'M158');
+            pause(turntime)
+            x = x + R*cosd(rot+90)+R*cosd(rot-90+((turntimetheo*speedcirkel*360)/(2*pi*R)));
+            y = y + R*sind(rot+90)+R*sind(rot-90+((turntimetheo*speedcirkel*360)/(2*pi*R)));
+        elseif lr == 1 %turn right
+            EPOCommunications('transmit', 'D100');
+            EPOCommunications('transmit', 'M158');  
+            pause(turntime)
+            x = x + R*cosd(rot-90)+R*cosd(rot+90-((turntimetheo*speedcirkel*360)/(2*pi*R)));
+            y = y + R*sind(rot-90)+R*sind(rot+90-((turntimetheo*speedcirkel*360)/(2*pi*R)));
+        else %no turn
+        end  
+            EPOCommunications('transmit', 'M143');
+            pause(0.3)
+            EPOCommunications('transmit', 'M150');
+        [x,y]=tdoatest(x,y,ref);
+        if (checkdistance(x,y,xdest,ydest)==1)
+           return; 
+        end
+    if (challenge==3)
+        [x,y,orientation]=Objectontwijk(x,y,rot)
+    end
+        xrota=x;
+        yrota=y;
+
+        EPOCommunications('transmit', 'D150');
+        EPOCommunications('transmit', 'M158');
+        pause(1.5)
+        %drive forward
+        x = x + timetheorecht*speedrecht*cosd(orientation)
+        y = y + timetheorecht*speedrecht*sind(orientation)     
+            EPOCommunications('transmit', 'M143');
+            pause(0.3)
+            EPOCommunications('transmit', 'M150');
+        [x,y,orientation]=tdoatest2(x,y,orientation,xrota,yrota,ref);
+        if (checkdistance(x,y,xdest,ydest)==1)
+           return; 
+        end
+    if (challenge==3)
+        [x,y,orientation]=Objectontwijk(x,y,rot)
+    end
+        
+        rot=orientation;
+        [turntime,turntimetheo,orientation,lr,straighttime,straighttimetheo,OoF] = control(x,y,rot,xdest,ydest,curve);
+    end
+
+%turn and drive
+if lr == -1 %turn left
+        EPOCommunications('transmit', 'D200');
+        EPOCommunications('transmit', 'M158');
+    pause(turntime)
+    x = x + R*cosd(rot+90)+R*cosd(rot-90+((turntimetheo*speedcirkel*360)/(2*pi*R)));
+    y = y + R*sind(rot+90)+R*sind(rot-90+((turntimetheo*speedcirkel*360)/(2*pi*R)));
+elseif lr == 1 %turn right
+        EPOCommunications('transmit', 'D100');
+        EPOCommunications('transmit', 'M158');  
+    pause(turntime)
+    x = x + R*cosd(rot-90)+R*cosd(rot-90-((turntimetheo*speedcirkel*360)/(2*pi*R)));
+    y = y + R*sind(rot-90)+R*sind(rot-90-((turntimetheo*speedcirkel*360)/(2*pi*R)));
+else %no turn
+end
+            EPOCommunications('transmit', 'M143');
+            pause(0.3)
+            EPOCommunications('transmit', 'M150');
+        [x,y]=tdoatest(x,y,ref)
+        if (checkdistance(x,y,xdest,ydest)==1)
+           return; 
+        end
+    if (challenge==3)
+        [x,y,orientation]=Objectontwijk(x,y,rot);
+    end
+        xrota=x;
+        yrota=y;
+        EPOCommunications('transmit', 'D150');
+        EPOCommunications('transmit', 'M158');
+pause(straighttime)
+x = x + straighttimetheo*speedrecht*cosd(orientation);
+y = y + straighttimetheo*speedrecht*sind(orientation);
+        EPOCommunications('transmit', 'M143');
+        pause(0.3)
+        EPOCommunications('transmit', 'M150');
+        [x,y,orientation]=tdoatest2(x,y,orientation,xrota,yrota,ref);
+        if (checkdistance(x,y,xdest,ydest)==1)
+           return; 
+        end
+
+function [x,y]=tdoatest(x,y,ref)
+%     xtdoa = x%+((rand-0.5)/4);
+%     ytdoa = y%+((rand-0.5)/4);
+          EPOCommunications('transmit','A1');
+          inputbuffer=audio_recieve();
+          EPOCommunications('transmit','A0');
+        [r12,r13,r14,r23,r24,r34] = TDOA(inputbuffer(:,1),inputbuffer(:,2),inputbuffer(:,3),inputbuffer(:,4),ref);
+        [xtdoa,ytdoa] = linnie(r12,r14,r23,r34);
+    if not(isnan(xtdoa)||isnan(ytdoa))
+        plot(x,y,'c*')
+        plot(xtdoa,ytdoa,'x')
+        if sqrt((x-xtdoa)^2+(y-ytdoa)^2)<0.7
+            x=xtdoa;
+            y=ytdoa;
+        end
+    end
+    
+function [x,y,orientation]=tdoatest2(x,y,orientation, xrota,yrota,ref)
+%     xtdoa = x%+((rand-0.5)/4);
+%     ytdoa = y%+((rand-0.5)/4);
+          EPOCommunications('transmit','A1');
+          inputbuffer=audio_recieve();
+          EPOCommunications('transmit','A0')
+        [r12,r13,r14,r23,r24,r34] = TDOA(inputbuffer(:,1),inputbuffer(:,2),inputbuffer(:,3),inputbuffer(:,4),ref);
+        [xtdoa,ytdoa] = linnie(r12,r14,r23,r34)
+        
+        plot(x,y,'c*')
+        plot(xtdoa,ytdoa,'x')
+        if sqrt((x-xtdoa)^2+(y-ytdoa)^2)<0.7
+            x=xtdoa;
+            y=ytdoa;
+        else
+            EPOCommunications('transmit','A1');
+            inputbuffer=audio_recieve();
+            EPOCommunications('transmit','A0')
+            [r12,r13,r14,r23,r24,r34] = TDOA(inputbuffer(:,1),inputbuffer(:,2),inputbuffer(:,3),inputbuffer(:,4),ref);
+            [xtdoa,ytdoa] = linnie(r12,r14,r23,r34)
+            plot(x,y,'c*')
+            plot(xtdoa,ytdoa,'x')
+            if sqrt((x-xtdoa)^2+(y-ytdoa)^2)<0.7
+                x=xtdoa;
+                y=ytdoa;
+            end
+        end
+        xrotb=x;
+        yrotb=y;
+        if sqrt((xrota-xrotb)^2+(yrota-yrotb)^2)>0.3
+            if xrota<xrotb
+            orientation = real(atand((yrota-yrotb)/(xrota-xrotb)))
+            else
+            orientation = real(atand((yrota-yrotb)/(xrota-xrotb))+180)
+            end
+        end
+            
+function arrived = checkdistance(x,y,xdest,ydest)
+    if sqrt((x-xdest)^2+(y-ydest)^2)<.2
+        arrived = 1;
+    else
+        arrived = 0;
+    end 
